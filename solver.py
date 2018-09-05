ceb908bcd4a6ee1be271afbd68d310d334076a50
Matan Noam Shavit <matannoam@gmail.com>
2018-09-05 14:21:21 -0400

    smaller solver methods

--- solver.py
+++ solver.py
@@ -14,49 +14,55 @@ class Solver(object):
         return self.board
 
     def changes_one_board_pass(self):
-        changed = False
-        for i in xrange(9):
-            for j in xrange(9):
-                if self.board[i][j] is None:
-                    if self.changes_space(i, j):
-                        changed = True
-        return changed
+        return any(
+            self.sets_space(i, j)
+            for i in xrange(9)
+            for j in xrange(9))
 
-    def changes_space(self, i, j):
-        missing_from_row = self.missing(self.row_for_space(i, j))
-        missing_from_column = self.missing(self.column_for_space(i, j))
-        missing_from_nonant = self.missing(self.nonant_for_space(i, j))
+    def sets_space(self, i, j):
+        if self.board[i][j] is None:
+            if self.sets_board_if_only_one_possible(i, j):
+                return True
+        return False
 
-        missing_from_all = [
+    def possibilities(self, i, j):
+        missing_from_row = self.missing(self.row_indices_for_space(i, j))
+        missing_from_column = self.missing(self.column_indices_for_space(i, j))
+        missing_from_nonant = self.missing(self.nonant_indices_for_space(i, j))
+
+        return [
             digit for digit in missing_from_row
             if digit in missing_from_column and
             digit in missing_from_nonant]
 
-        if len(missing_from_all) == 1:
-            self.board[i][j] = missing_from_all[0]
+    def sets_board_if_only_one_possible(self, i, j):
+        possibilities = self.possibilities(i, j)
+        if len(possibilities) == 1:
+            self.board[i][j] = possibilities[0]
             return True
         else:
             return False
 
-    def row_for_space(self, i, j):
-        return self.board[i]
+    def row_indices_for_space(self, i, j):
+        return [(i, k) for k in xrange(9)]
 
-    def column_for_space(self, i, j):
-        return [board_row[j] for board_row in self.board]
+    def column_indices_for_space(self, i, j):
+        return [(k, j) for k in xrange(9)]
 
-    def nonant_for_space(self, i, j):
+    def nonant_indices_for_space(self, i, j):
         nonant_x = (i / 3) * 3
         nonant_y = (j / 3) * 3
 
-        nonant = []
-        for x in xrange(nonant_x, nonant_x + 3):
-            for y in xrange(nonant_y, nonant_y + 3):
-                nonant.append(self.board[x][y])
+        return [
+            (x, y)
+            for x in xrange(nonant_x, nonant_x + 3)
+            for y in xrange(nonant_y, nonant_y + 3)]
 
-        return nonant
-
-    def missing(self, portion):
+    def missing(self, indices):
         one_to_nine = range(1, 10)
         return [
             digit for digit in one_to_nine
-            if digit not in portion]
+            if digit not in self.board_spaces(indices)]
+
+    def board_spaces(self, indices):
+        return [self.board[i[0]][i[1]] for i in indices]
