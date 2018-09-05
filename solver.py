fdc5b771846d8e2ea1ce054343d3006988ed4e39
Matan Noam Shavit <matannoam@gmail.com>
2018-09-05 11:37:58 -0400

    use solve for each space

--- solver.py
+++ solver.py
@@ -6,21 +6,26 @@ class Solver(object):
     def solve(self):
         for i in xrange(9):
             for j in xrange(9):
-                missing_from_row = self.missing(self.row(i, j))
-                missing_from_column = self.missing(self.column(i, j))
-                missing_from_both = [
-                    digit for digit in missing_from_row
-                    if digit in missing_from_column]
-
-                if len(missing_from_both) == 1:
-                    self.board[i][j] = missing_from_both[0]
+                if self.board[i][j] is None:
+                    self.solve_space(i, j)
 
         return self.board
 
-    def row(self, i, j):
+    def solve_space(self, i, j):
+        missing_from_row = self.missing(self.row_for_space(i, j))
+        missing_from_column = self.missing(self.column_for_space(i, j))
+
+        missing_from_all = [
+            digit for digit in missing_from_row
+            if digit in missing_from_column]
+
+        if len(missing_from_all) == 1:
+            self.board[i][j] = missing_from_all[0]
+
+    def row_for_space(self, i, j):
         return self.board[i]
 
-    def column(self, i, j):
+    def column_for_space(self, i, j):
         return [board_row[j] for board_row in self.board]
 
     def missing(self, portion):
