241d002d3d707190ac365f3b63f93523334ab592
Matan Noam Shavit <matannoam@gmail.com>
2018-09-05 10:45:12 -0400

    find one digit on rows

--- solver.py
+++ solver.py
@@ -4,4 +4,17 @@ class Solver(object):
         self.board = board
 
     def solve(self):
+        for i, row in enumerate(self.board):
+            missing_from_row = self.missing(row)
+            if len(missing_from_row) == 1:
+                self.board[i] = [
+                    digit if digit else missing_from_row[0]
+                    for digit in row]
+
         return self.board
+
+    def missing(self, portion):
+        one_to_nine = range(1, 10)
+        return [
+            digit for digit in one_to_nine
+            if digit not in portion]
--- test_solver.py
+++ test_solver.py
@@ -23,3 +23,10 @@ class SolverTests(TestCase):
         initial_board = deepcopy(self.solution)
         solver = Solver(initial_board)
         self.assertEqual(solver.solve(), self.solution)
+
+    def test_missing_single_digits_from_rows(self):
+        initial_board = deepcopy(self.solution)
+        for row in initial_board:
+            row[0] = None
+        solver = Solver(initial_board)
+        self.assertEqual(solver.solve(), self.solution)
