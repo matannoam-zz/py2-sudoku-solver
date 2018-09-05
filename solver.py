44cef5ab20bb89720ecdb0dcb95aea69c4406bad
Matan Noam Shavit <matannoam@gmail.com>
2018-09-05 10:43:45 -0400

    solve a solved board

---
+++ solver.py
@@ -0,0 +1,7 @@
+class Solver(object):
+
+    def __init__(self, board):
+        self.board = board
+
+    def solve(self):
+        return self.board
---
+++ test_solver.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from copy import deepcopy
+
+from solver import Solver
+
+
+class SolverTests(TestCase):
+
+    def setUp(self):
+        self.solution = [
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [4, 5, 6, 7, 8, 9, 1, 2, 3],
+            [7, 8, 9, 1, 2, 3, 4, 5, 6],
+            [2, 3, 1, 5, 6, 4, 8, 9, 7],
+            [5, 6, 4, 8, 9, 7, 2, 3, 1],
+            [8, 9, 7, 2, 3, 1, 5, 6, 4],
+            [3, 1, 2, 6, 4, 5, 9, 7, 8],
+            [6, 4, 5, 9, 7, 8, 3, 1, 2],
+            [9, 7, 8, 3, 1, 2, 6, 4, 5]]
+
+    def test_returns_input_for_solved_puzzle(self):
+        initial_board = deepcopy(self.solution)
+        solver = Solver(initial_board)
+        self.assertEqual(solver.solve(), self.solution)
