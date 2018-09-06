79d70f6f7833095985c46092e932ba08e58b71f9
Matan Noam Shavit <matannoam@gmail.com>
2018-09-06 10:05:31 -0400

    possibilities class in own file

--- solver/main.py
+++ solver/main.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 
 from .indices import Indices
+from .possibilities import Possibilties
 
 
 class Solver(object):
@@ -65,37 +66,3 @@ class Utilities(object):
         return all(
             cls.in_any(item, items_groups)
             for items_groups in groups_of_groups)
-
-
-class Possibilties(object):
-
-    def __init__(self, board):
-        self.board = board
-
-    def get_by_list(self, indices):
-        return [self.get(i[0], i[1]) for i in indices]
-
-    def get(self, i, j):
-        if self.board[i][j]:
-            return [self.board[i][j]]
-
-        row_indicies = Indices.row_indices_for_space(i, j)
-        column_indicies = Indices.column_indices_for_space(i, j)
-        nonant_indicies = Indices.nonant_indices_for_space(i, j)
-
-        return list(self.missing_from_all(
-            [row_indicies, column_indicies, nonant_indicies]))
-
-    def missing_from_all(self, groups_of_indices):
-        missing_from_groups = [
-            self.missing(indices) for indices in groups_of_indices]
-        return set.intersection(*missing_from_groups)
-
-    def missing(self, indices):
-        one_to_nine = range(1, 10)
-        return set(
-            digit for digit in one_to_nine
-            if digit not in self.board_spaces(indices))
-
-    def board_spaces(self, indices):
-        return [self.board[i[0]][i[1]] for i in indices]
