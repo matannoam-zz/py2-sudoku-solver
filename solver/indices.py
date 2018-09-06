1100de1b6f62eee27e4b07d638a9fe962099b78a
Matan Noam Shavit <matannoam@gmail.com>
2018-09-06 10:03:48 -0400

    indices class in own file

---
+++ solver/indices.py
@@ -0,0 +1,38 @@
+class Indices(object):
+
+    @classmethod
+    def row_indices_for_space(cls, i, j):
+        return [(i, k) for k in xrange(9)]
+
+    @classmethod
+    def column_indices_for_space(self, i, j):
+        return [(k, j) for k in xrange(9)]
+
+    @classmethod
+    def nonant_indices_for_space(self, i, j):
+        nonant_x = (i / 3) * 3
+        nonant_y = (j / 3) * 3
+
+        return [
+            (x, y)
+            for x in xrange(nonant_x, nonant_x + 3)
+            for y in xrange(nonant_y, nonant_y + 3)]
+
+    @classmethod
+    def row_indices_without_space(cls, i, j):
+        return [(i, k) for k in xrange(9) if k != j]
+
+    @classmethod
+    def column_indices_without_space(self, i, j):
+        return [(k, j) for k in xrange(9) if k != i]
+
+    @classmethod
+    def nonant_indices_without_space(self, i, j):
+        nonant_x = (i / 3) * 3
+        nonant_y = (j / 3) * 3
+
+        return [
+            (x, y)
+            for x in xrange(nonant_x, nonant_x + 3)
+            for y in xrange(nonant_y, nonant_y + 3)
+            if x != i or y != j]
--- solver/main.py
+++ solver/main.py
@@ -1,5 +1,7 @@
 from copy import deepcopy
 
+from .indices import Indices
+
 
 class Solver(object):
 
@@ -97,43 +99,3 @@ class Possibilties(object):
 
     def board_spaces(self, indices):
         return [self.board[i[0]][i[1]] for i in indices]
-
-
-class Indices(object):
-
-    @classmethod
-    def row_indices_for_space(cls, i, j):
-        return [(i, k) for k in xrange(9)]
-
-    @classmethod
-    def column_indices_for_space(self, i, j):
-        return [(k, j) for k in xrange(9)]
-
-    @classmethod
-    def nonant_indices_for_space(self, i, j):
-        nonant_x = (i / 3) * 3
-        nonant_y = (j / 3) * 3
-
-        return [
-            (x, y)
-            for x in xrange(nonant_x, nonant_x + 3)
-            for y in xrange(nonant_y, nonant_y + 3)]
-
-    @classmethod
-    def row_indices_without_space(cls, i, j):
-        return [(i, k) for k in xrange(9) if k != j]
-
-    @classmethod
-    def column_indices_without_space(self, i, j):
-        return [(k, j) for k in xrange(9) if k != i]
-
-    @classmethod
-    def nonant_indices_without_space(self, i, j):
-        nonant_x = (i / 3) * 3
-        nonant_y = (j / 3) * 3
-
-        return [
-            (x, y)
-            for x in xrange(nonant_x, nonant_x + 3)
-            for y in xrange(nonant_y, nonant_y + 3)
-            if x != i or y != j]
